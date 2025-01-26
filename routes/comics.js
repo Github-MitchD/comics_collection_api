95935b885ac9243572e7a05c5ed7e84ece74cfac
Michel Dufour <hello@michel-dufour.fr>
2025-01-26 16:48:35 +0100

    Ajout de la route PUT pour mettre à jour un comic par ID dans `routes/comics.js`

--- routes/comics.js
+++ routes/comics.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllComics, createComic, getComicById } = require('../controllers/comicsController');
+const { getAllComics, createComic, getComicById, updateComic } = require('../controllers/comicsController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/', getAllComics);
 router.get('/:id', getComicById);
 router.post('/', authMiddleware, createComic);
+router.put('/:id', authMiddleware, updateComic);
 
 module.exports = router;
