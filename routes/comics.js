2f64a903416bc2b6893befd4ffd1a5d0d4c34e63
Michel Dufour <hello@michel-dufour.fr>
2025-01-26 17:24:46 +0100

    Ajout de la route de suppression pour les comics

--- routes/comics.js
+++ routes/comics.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllComics, createComic, getComicById, updateComic } = require('../controllers/comicsController');
+const { getAllComics, createComic, getComicById, updateComic, deleteComic } = require('../controllers/comicsController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get('/', getAllComics);
 router.get('/:id', getComicById);
 router.post('/', authMiddleware, createComic);
 router.put('/:id', authMiddleware, updateComic);
+router.delete('/:id', authMiddleware, deleteComic);
 
 module.exports = router;
