ef3231a0857768929632be07bdd598b94507fe90
Michel Dufour <hello@michel-dufour.fr>
2025-01-26 03:23:47 +0100

    Ajout du contrôleur `authController` : gestion de l'enregistrement et de la connexion des utilisateurs avec validation des données

---
+++ controllers/authController.js
@@ -0,0 +1,37 @@
+const { User } = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+exports.register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = await User.create({ username, email, password });
+        res.status(201).json({ message: 'User registered successfully', user });
+    } catch (error) {
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            return res.status(400).json({ message: 'This email already exists.' });
+        }
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid user' });
+        }
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
