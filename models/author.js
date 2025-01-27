fab53096a33345daa956ae0800f59d31db4c1885
Michel Dufour <hello@michel-dufour.fr>
2025-01-27 04:00:39 +0100

    Ajout du modèle `Author` avec validation des champs et association avec les comics

---
+++ models/author.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Author = sequelize.define('Author', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: { msg: "The name is required." },
+                notNull: { msg: "The name is required." },
+            }
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        birthdate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            validate: {
+                isDate: { msg: "Must be a valid date." }
+            }
+        },
+        bio: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        website: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isUrl: { msg: "Must be a valid URL." }
+            }
+        }
+    });
+
+    Author.associate = (models) => {
+        Author.hasMany(models.Comic, { foreignKey: 'authorId', as: 'comics' });
+    };
+
+    return Author;
+};
